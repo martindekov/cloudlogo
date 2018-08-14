bca2cd76e6cc91ec0779700468d4d7088067cce5
Martin Dekov (VMware) <mdekov@vmware.com>
2018-08-14 01:56:02 -0700

    wat

--- clouddemo/handler.go
+++ clouddemo/handler.go
@@ -3,6 +3,6 @@ package function
 // Handle a serverless request
 func Handle(req []byte) string {
 	//return fmt.Sprintf("Hello, Go. You said: %s", string(req))
-	html := `<img src="https://www.openfaas.com/images/openfaas/logo.png" alt="Paris" class="center">`
+	html := `<img src="https://www.openfaas.com/images/openfaas/logo.png" alt="Paris" style="display: block;margin-left: auto;margin-right: auto;width:50%;">`
 	return html
 }
