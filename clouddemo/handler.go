3e158fac2d680b2cec617e24396a3ebff4949213
Martin Dekov (VMware) <mdekov@vmware.com>
2018-08-14 03:26:10 -0700

    nothing

--- clouddemo/handler.go
+++ clouddemo/handler.go
@@ -18,8 +18,8 @@ func Handle(req []byte) string {
 	html := `
 		<body style="background-color:#B0D0D3">
 			<div style="display: block;margin-left: auto;margin-right: auto;width:50%;overflow:hidden">
-				<img src="https://www.openfaas.com/images/openfaas/logo.png" style="display:block">
-				<img src="https://irp-cdn.multiscreensite.com/0e5c5b66/dms3rep/multi/tablet/animated-cloud-right-to-left-350x211-350x211.png" style="z-index:5;height:250px;position:relative;top:-70px;left:-20px;display:block">
+				<img src="https://www.openfaas.com/images/openfaas/logo.png" style="display:block;margin:0">
+				<img src="https://irp-cdn.multiscreensite.com/0e5c5b66/dms3rep/multi/tablet/animated-cloud-right-to-left-350x211-350x211.png" style="z-index:5;height:250px;position:relative;top:-70px;left:-20px;display:block;margin:0">
 			</div>
 		</body>
 		`
