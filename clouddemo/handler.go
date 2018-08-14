8b4549f09b4d75c337707fc2bb75170c5f437f2d
Martin Dekov (VMware) <mdekov@vmware.com>
2018-08-14 02:19:09 -0700

    b

--- clouddemo/handler.go
+++ clouddemo/handler.go
@@ -4,11 +4,11 @@ package function
 func Handle(req []byte) string {
 	//return fmt.Sprintf("Hello, Go. You said: %s", string(req))
 	html := `
-			<div style="background-color:#B0D0D3">
+			<body style="background-color:#B0D0D3">
 				<div>
 					<img src="https://www.openfaas.com/images/openfaas/logo.png" alt="Paris" style="display: block;margin-left: auto;margin-right: auto;width:50%;">
 				</div>
-			</div>
+			</body>
 			`
 	return html
 }
