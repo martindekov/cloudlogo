caddb1d1f8680d1db005312415f3f4d24f383ea4
Martin Dekov (VMware) <mdekov@vmware.com>
2018-08-14 03:21:55 -0700

    hopes

--- clouddemo/handler.go
+++ clouddemo/handler.go
@@ -3,15 +3,25 @@ package function
 // Handle a serverless request
 func Handle(req []byte) string {
 	//return fmt.Sprintf("Hello, Go. You said: %s", string(req))
-	html := `
-			<body style="background-color:#B0D0D3">
-				<div style="display: block;margin-left: auto;margin-right: auto;width:50%;position=absolute;">
-					<div style="position:relative;margin:50%">
-						<img src="https://www.openfaas.com/images/openfaas/logo.png">
-						<img src="https://irp-cdn.multiscreensite.com/0e5c5b66/dms3rep/multi/tablet/animated-cloud-right-to-left-350x211-350x211.png" style="z-index:5;height:250px;position:relative;top:-70px;left:-20px;">
+	/*
+		html := `
+				<body style="background-color:#B0D0D3">
+					<div style="display: block;margin-left: auto;margin-right: auto;width:50%;position=absolute;">
+						<div style="position:relative;margin:50%">
+							<img src="https://www.openfaas.com/images/openfaas/logo.png">
+							<img src="https://irp-cdn.multiscreensite.com/0e5c5b66/dms3rep/multi/tablet/animated-cloud-right-to-left-350x211-350x211.png" style="z-index:5;height:250px;position:relative;top:-70px;left:-20px;">
+						</div>
 					</div>
-				</div>
-			</body>
-			`
+				</body>
+				`
+	*/
+	html := `
+		<body style="background-color:#B0D0D3">
+			<div style="display: block;margin-left: auto;margin-right: auto;width:50%;overflow:hidden">
+				<img src="https://www.openfaas.com/images/openfaas/logo.png" style="display:block">
+				<img src="https://irp-cdn.multiscreensite.com/0e5c5b66/dms3rep/multi/tablet/animated-cloud-right-to-left-350x211-350x211.png" style="z-index:5;height:250px;position:relative;top:-70px;left:-20px;display:block">
+			</div>
+		</body>
+		`
 	return html
 }
