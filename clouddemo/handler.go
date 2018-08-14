1759fc061b1ebe4ffa8327f921371daaf256bcce
Martin Dekov (VMware) <mdekov@vmware.com>
2018-08-14 01:41:56 -0700

    wat

--- clouddemo/handler.go
+++ clouddemo/handler.go
@@ -1,10 +1,8 @@
 package function
 
-import (
-	"fmt"
-)
-
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+	//return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+	html := `<h1>WAT</h1>`
+	return html
 }
