5e05e96dc534acee44646058e52b1826bb47807d
Martin Dekov (VMware) <mdekov@vmware.com>
2018-08-14 01:49:19 -0700

    idk

--- clouddemo/handler.go
+++ clouddemo/handler.go
@@ -3,6 +3,6 @@ package function
 // Handle a serverless request
 func Handle(req []byte) string {
 	//return fmt.Sprintf("Hello, Go. You said: %s", string(req))
-	html := `<h1><div text-align:center>WAT</div></h1>`
+	html := `<h1><div style="width:800px; margin:0 auto;">WAT</div></h1>`
 	return html
 }
