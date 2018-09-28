8ccbdc04da6a90cb38d15b367da138be66e5c7d9
Martin Dekov <34942004+martindekov@users.noreply.github.com>
2018-09-28 14:47:21 +0300

    Update handler.go

--- clouddemo/handler.go
+++ clouddemo/handler.go
@@ -20,7 +20,7 @@ func Handle(req []byte) string {
 	html := `
 		<body style="display:table;position:absolute;height:100%;background-color:#276ddd;width:100%;">
 			<div style="display: table-cell;vertical-align: middle;">
-				<div style="margin-left: auto;margin-right: auto;">
+				<div style="margin-left: auto;margin-right: auto;"> 
 					<img src="https://www.openfaas.com/images/openfaas/openfaas.png" style="display:block;margin:auto">
 					<img src="https://www.openfaas.com/images/openfaas/logo.png" style="display:block;margin:auto">
 					<img src="https://irp-cdn.multiscreensite.com/0e5c5b66/dms3rep/multi/tablet/animated-cloud-right-to-left-350x211-350x211.png" style="z-index:5;height:250px;position:relative;top:-70px;left:-10px;display:block;margin:auto">
