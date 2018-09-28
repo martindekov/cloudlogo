ebd88ecbea68be6ad4cd07d4e2bf2877e146f21e
Martin Dekov <34942004+martindekov@users.noreply.github.com>
2018-09-28 11:24:33 +0300

    Update handler.go

--- clouddemo/handler.go
+++ clouddemo/handler.go
@@ -3,7 +3,6 @@ package function
 // Handle a serverless request
 func Handle(req []byte) string {
 	color := true
-	
 	if color {
 		html := `
 		<body style="display:table;position:absolute;height:100%;background-color:#B0D0D3;width:100%;">
